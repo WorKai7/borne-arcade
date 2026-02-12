5dd84b58ac51e7a9c77f7f93134ef0d4f5d79cad
WorKai7 <jvcraft59@gmail.com>
2026-02-12 10:51:50 +0100

    fix(games): Fixed all games

--- git/borne_arcade/Pong.sh
+++ git/borne_arcade/Pong.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 xdotool mousemove 1280 1024
-cd projet/Pong
-java -cp .:../..:/home/pi/git/MG2D Main
+java -cp .:../..:/home/pi/git/MG2D projet.Pong.Main
--- git/borne_arcade/Snake_Eater.sh
+++ git/borne_arcade/Snake_Eater.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 xdotool mousemove 1280 1024
-cd projet/Snake_Eater
 touch highscore
-java -cp .:../..:/home/pi/git/MG2D Snake_Eater
+java -cp .:../..:/home/pi/git/MG2D projet.Snake_Eater.Snake_Eater
